1ee5b23dffb05b857b048e2629db7e9dc45e0516
Kelly Wang <kwang@kwang-mba.local>
2013-08-01 15:06:11 -0700

    json stuff in index

--- app/controllers/maraudersmap_controller.rb
+++ app/controllers/maraudersmap_controller.rb
@@ -1,7 +1,10 @@
+require 'open-uri'
+
 class MaraudersmapController < ApplicationController
   def index
    	url = "https://net-util4001.ecr.box.net:4743/network/wireless_associations.json"
-   	file = File.open(url, "rb")
-   	@result = file.read
+   	file = open(url)
+   	@contents = file.read
+   	
   end
 end
