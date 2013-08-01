d3ee0ce004cf69e36e2ecbd1855cffdcd29072cf
Kelly Wang <kwang@kwang-mba.local>
2013-08-01 15:53:31 -0700

    still working

--- app/controllers/maraudersmap_controller.rb
+++ app/controllers/maraudersmap_controller.rb
@@ -1,10 +1,14 @@
 require 'open-uri'
+require 'rubygems'
+require 'json'
 
 class MaraudersmapController < ApplicationController
   def index
-   	url = "https://net-util4001.ecr.box.net:4743/network/wireless_associations.json"
-   	file = open(url)
-   	@contents = file.read
-   	
+   	@url = "https://net-util4001.ecr.box.net:4743/network/wireless_associations.json"
+   	@file = open(@url)
+   	@contents = @file.read
+   	@parsed = JSON.parse(@contents)
+   	@parsed.each {}
+   	@locations = {}
   end
 end
