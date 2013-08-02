31a2bd5e7875a39180b632d1ccc390896139d48d
Malika Rajvanshy <mrajvanshy@mrajvanshy-mba.local>
2013-08-01 17:10:10 -0700

    separating floors

--- app/controllers/maraudersmap_controller.rb
+++ app/controllers/maraudersmap_controller.rb
@@ -3,20 +3,38 @@ require 'rubygems'
 require 'json'
 
 class MaraudersmapController < ApplicationController
+  before_action :index
+
   def index
    	@url = "https://net-util4001.ecr.box.net:4743/network/wireless_associations.json"
    	@file = open(@url)
    	@contents = @file.read
 
    	@parsed = JSON.parse(@contents)
-   	@users = Hash.new
+   	$users = Hash.new
+   	$hubs = Hash.new
+
     @parsed.each do |item|
     	username = item[1]["username"]
     	ap = item[1]["ap"]
-    	if (is_ecr(ap) && !is_guest(username))
-    		@users[username] = ap
+    	if (is_ecr(ap) && !is_guest(username) && is_alphanumeric(username))
+    		$users[username] = ap
+    		if ($hubs[ap].nil?)
+    			$hubs[ap] = Array.new
+    		end
+    		$hubs[ap] << username
     	end
    	end
+  end
+
+  def first
+  	@foo = "hello"
+  end
+
+  def second
+  end
+
+  def third
   end
 
   private
@@ -26,5 +44,9 @@ class MaraudersmapController < ApplicationController
 
   def is_guest(username)
   	return ((username.nil?) || (username == "\"\""))
+  end
+
+  def is_alphanumeric(username)
+  	return username.match(/^[[:alpha:]]+$/)
   end
 end
